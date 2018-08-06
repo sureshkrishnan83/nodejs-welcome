a99fad18f7dfa175155e2e70da0c511acf9631a6
SureshKrishnan <sureshkrishnan83@outlook.com>
2018-08-06 12:31:45 +0800

    change text from hello to apac wintel

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var healthy=true;
 
 
 app.get('/', function (req, res) {
-  res.send('Hello world v.1.14 ' + os.hostname() + '\n');
+  res.send('Hello APAC WINTEL v.1.14 ' + os.hostname() + '\n');
 });
 
 
